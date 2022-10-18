79095b80a7a894ec0ca123780c5e6a95ae880161
Ipoops <ipmasterze@gmail.com>
2022-10-18 13:05:28 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,20 @@
-#include <main.h>
+#include "main.h"
 
+/**
+ * main - print _putchar
+ *
+ * Return: 0. Always(Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
