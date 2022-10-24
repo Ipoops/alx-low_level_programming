4805b0fc256e9db7e7c79919f341b9a38cc2a0cb
Ipoops <ipmasterze@gmail.com>
2022-10-24 10:22:07 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+iiiiiiiiii
+/**
+ * _puts - prints a string followed y a newline
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
