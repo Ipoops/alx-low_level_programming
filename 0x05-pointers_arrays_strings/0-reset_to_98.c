d22289cbc64b94e9949cf9b53816d2df7806897e
Ipoops <ipmasterze@gmail.com>
2022-10-25 01:59:26 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - int *n updates the address of n
- *
- * Return: Always 0
+ * reset_to_98 - update the value of n to 98
+ * @n: input
+ * Return: n
  */
 void reset_to_98(int *n)
 {
