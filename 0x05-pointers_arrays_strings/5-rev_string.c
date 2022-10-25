e3302c2be28abde590f1bc996616be5e6d2e7b61
Ipoops <ipmasterze@gmail.com>
2022-10-25 04:34:20 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,22 +2,31 @@
 
 /**
  * rev_string - reverses a string
- * @s: input
- * Retur: string in reverse
+ * @s: string to be reversed
  */
 void rev_string(char *s)
 {
-	char rv = s[0];
-	int c = 0;
-	int i;
+	char tmp;
 
-	while (s[c] != '\0')
-		c++;
-	for (i = 0; i < c; i++;)
+	int i, len, len1;
+
+
+	len = 0;
+
+	len1 = 0;
+
+
+	while (s[len] != '\0')
+
 	{
-		c--;
-		rv = s[i];
-		s[i] = s[c];
-		s[c] = rv;
+		len++;
+	}
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
