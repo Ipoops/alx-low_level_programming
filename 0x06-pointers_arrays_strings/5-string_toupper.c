da65a8f0f6339cdff35cb37c5af7fd1a074b59d0
Ipoops <ipmasterze@gmail.com>
2022-10-27 07:03:05 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + 1))
+	{
+		if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
+			*(s + 1) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
