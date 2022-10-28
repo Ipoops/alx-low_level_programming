3e9b866e3bad9821d0ffdd476eb4ebd27207334b
Ipoops <ipmasterze@gmail.com>
2022-10-27 20:30:27 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,28 +3,32 @@
 /**
  * cap_string - capitalizes all words in a string
  * @s:string
- * Return: address of s
+ * Return: char pointer
  */
 char *cap_string(char *s)
 {
 	int i = 0, j;
-	char a[] = "'\t\n,;.!?"() {} ";
+	char sept[] = {32, '\t', 11, '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (*(s + i)
+
+	if (s[0] > 96 && s[0] < 123)
+		s[0] -= 32;
+	while (s[i] != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if (s[i] > 96 && s[i] < 123)
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
+			j = 0;
+			while (j < 14)
 			{
-				for (j = 0; j <= 12; j++)
+				if (s[i - 1] == sept[j])
 				{
-					if (a[j] == *(s + i - 1))
+					s[i] -= 32;
+					break;
 				}
+				j++;
 			}
 		}
 		i++;
 	}
-	return(s);
+	return (s);
 }
