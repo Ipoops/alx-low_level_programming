a5beb50f18f3e6efa34715592e3f85d6fc8e6f57
Ipoops <ipmasterze@gmail.com>
2022-11-01 06:26:35 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,19 +7,19 @@
  *
  * Return: pointer to byte s or NULL
  */
-char *s_strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int index;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (index = 0; accept[index]; index++)
 		{
-			if (s[i] == accept[j])
-			{
-				return (&s[i]);
-			}
+			if (*s == accept[index])
+				return (s);
 		}
+
+		s++;
 	}
-	return (NULL);
+	return ('\0');
 }
