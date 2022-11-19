30707004f7e9a58410a3a30479fc829d998c1adb
Ipoops <ipmasterze@gmail.com>
2022-11-19 01:16:17 -0800

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,10 @@
-fndef F_POINTER
+#ifndef F_POINTER
 #define F_POINTER
 #include <stdio.h>
 #include <stdlib.h>
-int_putchar(char c);
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *c));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array. int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
