1a659d4d3557dd90b98672bdddcc8e387cab4406
Ipoops <ipmasterze@gmail.com>
2022-11-19 01:58:33 -0800

    task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator -acts on each array member
+ * @array: an array of characters
+ * @size: an int
+ * @action: pointer to a function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
